a5c5a037dc0f2bac7668f6a4d239b95303a6c0db
manuelatrotta <manuelat88@hotmail.it>
2020-02-03 20:15:29 +0100

    stampo su schermo mese attuale e giorni

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
 //  alert('hello');
 actualDate = moment( '2018-' + '01-' + '01' );
-days = actualDate.daysInMonth();
-console.log(actualDate);
-console.log(days);
-actualMonth = actualDate.format('MMMM');
-console.log(actualMonth);
+  days = actualDate.daysInMonth();
+  console.log(actualDate);
+  console.log(days);
+ actualMonth = actualDate.format('MMMM');
+  console.log(actualMonth);
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  //scoperti i giorni nel mese serve un ciclo for per stamparli da 1 a 31
+  for (var i = 1; i <= 31; i++) {
+  $('.days').append(' '+ i + ' ');
+  }
+  $('.actualMonth').append(actualMonth);
 
 $.ajax(
   {
     'url':'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
     'method': 'GET',
-    'success': function (data) {
-      console.log(data);
+    'data': {
+      year: 2018,
+      month : 0,
+    },
+    'success': function(date,stato) {
+      console.log(date.response);
     },
     'error': function (request, state, errors) {
       alert('error' + errors);

f23ab53968c9de5308e112b88dda526773c65f2f
manuelatrotta <manuelat88@hotmail.it>
2020-02-03 12:22:15 +0100

    inizio esempi con moment.js per avere mese di gennaio 2018

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 //  alert('hello');
+
+var currentMonth = moment().format('YYYY-MM');
+console.log(currentMonth);
+
 $.ajax(
   {
     'url':'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
     'method': 'GET',
     'success': function (data) {
       console.log(data);
-
     },
     'error': function (request, state, errors) {
       alert('error' + errors);

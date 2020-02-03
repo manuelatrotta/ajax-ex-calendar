c45c13ca3236903b1eaf5430556d3f5ee8e59dd2
manuelatrotta <manuelat88@hotmail.it>
2020-02-03 12:01:10 +0100

    inizio inserimento chiamata

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
-  alert('hello');
+//  alert('hello');
+$.ajax(
+  {
+    'url':'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+    'method': 'GET',
+    'success': function (data) {
+      console.log(data);
+
+    },
+    'error': function (request, state, errors) {
+      alert('error' + errors);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
